943125c59892f825db46accfaa61451b20c21f18
duvan1026 <duvancastro1026@gmail.com>
2023-03-16 14:30:43 -0500

    ejecicio completado

---
+++ src/com/example/Main/Main.java
@@ -0,0 +1,19 @@
+package com.example.Main;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        int numero1 = 123;
+        long numero2 = 12345l;
+        double numero3 = 1233.2;
+        boolean state = false;
+        String cadena = "Este es una cadena de prueba";
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(state);
+        System.out.println(cadena);
+    }
+}
