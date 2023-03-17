7ef7ead7e46366a149a31720cd18f1fe13876787
duvan1026 <duvancastro1026@gmail.com>
2023-03-17 10:16:16 -0500

    implemented ejercice 3

--- src/com/ejercicios/tema3/ejerciciomain.java
+++ src/com/ejercicios/tema3/ejerciciomain.java
@@ -1,4 +1,19 @@
 package com.ejercicios.tema3;
 
 public class ejerciciomain {
+
+    public static void main(String[] args){
+
+        String[] nombres = {"Duvan", "Castro", "Bautista", "Dario"};
+        String nombreCompleto = "";
+        String espacio = " ";
+
+        for( int i = 0; i < nombres.length; i++ ){
+            nombreCompleto = nombreCompleto + espacio + nombres[i] ;
+        }
+
+        System.out.println( nombreCompleto );
+
+    }
+
 }
