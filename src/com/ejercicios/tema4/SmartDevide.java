a90db911e6f50d9e42017ce3613cdd1fe8ac9662
Duvan Castro <duvancastro1026@gmail.com>
2023-03-19 04:11:30 -0500

    created class

--- src/com/ejercicios/tema4/SmartDevide.java
+++ src/com/ejercicios/tema4/SmartDevide.java
@@ -1,4 +1,21 @@
 package com.ejercicios.tema4;
 
 public class SmartDevide {
+
+    String marca;
+    String modelo;
+    String Procesador;
+    int memoriaInterna;
+    boolean ResistenteAgua;
+
+    public SmartDevide(){
+    }
+
+    public SmartDevide(String marca, String modelo, String procesador, int memoriaInterna, boolean resistenteAgua) {
+        this.marca = marca;
+        this.modelo = modelo;
+        Procesador = procesador;
+        this.memoriaInterna = memoriaInterna;
+        ResistenteAgua = resistenteAgua;
+    }
 }
