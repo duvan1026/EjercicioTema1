de85fcb1bec1e240837b8739e0c9d824f8bf061b
Duvan Castro <duvancastro1026@gmail.com>
2023-03-19 04:19:51 -0500

    created class

--- src/com/ejercicios/tema4/SmartPhone.java
+++ src/com/ejercicios/tema4/SmartPhone.java
@@ -1,4 +1,26 @@
 package com.ejercicios.tema4;
 
-public class SmartPhone {
+public class SmartPhone extends SmartDevide{
+
+    String sistemaOperativo;
+    String resolucionPantalla;
+    int resolucionCamaraDelantera;
+    int getResolucionCamaraTrasera;
+    int memoriaRam;
+    boolean dualSim;
+    boolean detectorHuella;
+
+    public SmartPhone() {
+    }
+
+    public SmartPhone(String marca, String modelo, String procesador, int memoriaInterna, boolean resistenteAgua, String sistemaOperativo, String resolucionPantalla, int resolucionCamaraDelantera, int getResolucionCamaraTrasera, int memoriaRam, boolean dualSim, boolean detectorHuella) {
+        super(marca, modelo, procesador, memoriaInterna, resistenteAgua);
+        this.sistemaOperativo = sistemaOperativo;
+        this.resolucionPantalla = resolucionPantalla;
+        this.resolucionCamaraDelantera = resolucionCamaraDelantera;
+        this.getResolucionCamaraTrasera = getResolucionCamaraTrasera;
+        this.memoriaRam = memoriaRam;
+        this.dualSim = dualSim;
+        this.detectorHuella = detectorHuella;
+    }
 }
