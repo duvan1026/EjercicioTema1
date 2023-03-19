fb86b5e53d3a16da0ac6ce39bb2ad3a1da2e7953
Duvan Castro <duvancastro1026@gmail.com>
2023-03-19 04:38:44 -0500

    implemented class

--- src/com/ejercicios/tema4/main.java
+++ src/com/ejercicios/tema4/main.java
@@ -4,7 +4,11 @@ public class main {
 
     public static void main(String[] args){
 
+        SmartDevide xiaomi = new SmartPhone("Xiaomi","Redmi Note 11", "Qualcomm", 128, true, "Android", "FHD", 13,50,4,true,true);
+        System.out.println(xiaomi.marca + xiaomi.modelo);
 
+        xiaomi = new SmartWatch("Xiaomi","Redmi Watch 2 lite","Na",2,true,"TPU",'L',21);
+        System.out.println(xiaomi.marca + xiaomi.modelo + xiaomi.ResistenteAgua);
 
     }
 
