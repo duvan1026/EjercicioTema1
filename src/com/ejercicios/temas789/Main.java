fe3e62b07898c1302a976f35780cd27b33843e59
duvan1026 <duvancastro1026@gmail.com>
2023-03-23 14:10:33 -0500

    resolve exercise 2

--- src/com/ejercicios/temas789/Main.java
+++ src/com/ejercicios/temas789/Main.java
@@ -4,8 +4,24 @@ public class Main {
 
     public static void main(String[] args){
 
+        // Exercice 1
         String stringReverse = reverse("Hola Mundo");
         System.out.println(stringReverse);
+
+        // Exercice 2
+        String arrayBi[][] = new String[2][3];
+
+        arrayBi[0][0] = "Hola";
+        arrayBi[0][1] = "Mundo";
+        arrayBi[0][2] = "Tierra";
+
+        arrayBi[1][0] = "Duvan";
+        arrayBi[1][1] = "Castro";
+        arrayBi[1][2] = "bautista";
+
+        traverseArray(arrayBi);
+
+
     }
 
     public static String reverse(String texto){
@@ -18,5 +34,14 @@ public class Main {
 
         return cadenaInversa;
 
+    }
+
+    public static void traverseArray(String array[][]){
+
+        for(int i = 0; i < array.length; i++){
+            for(int j= 0; j < array[i].length; j++){
+                System.out.println(array[i][j]);
+            }
+        }
     }
 }
