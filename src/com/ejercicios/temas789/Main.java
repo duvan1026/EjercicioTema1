cefd4a506cbef8e6cc01acf63a4786596577bad3
duvan1026 <duvancastro1026@gmail.com>
2023-03-23 14:00:27 -0500

    resolve exercice 1

--- src/com/ejercicios/temas789/Main.java
+++ src/com/ejercicios/temas789/Main.java
@@ -1,4 +1,22 @@
 package com.ejercicios.temas789;
 
 public class Main {
+
+    public static void main(String[] args){
+
+        String stringReverse = reverse("Hola Mundo");
+        System.out.println(stringReverse);
+    }
+
+    public static String reverse(String texto){
+
+        String cadenaInversa = "";
+
+        for(int i = texto.length() - 1; i >= 0; i--){ // recorremos una cadena de texto
+            cadenaInversa += texto.charAt(i);
+        }
+
+        return cadenaInversa;
+
+    }
 }
